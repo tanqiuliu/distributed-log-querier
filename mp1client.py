ccbd070ed03eeaa35137a14d14f9338fdc75770d
Dehua Chen <dchen51@siebl-0218-12.ews.illinois.edu>
2018-09-12 15:27:42 -0500

    double checked if the client and server worked on one machine

--- mp1client.py
+++ mp1client.py
@@ -1,11 +1,17 @@
 import socket
+import sys
 
-HOST = ''
-PORT = 12345
+if __name__ == "__main__":
+	vmNum = sys.argv[1]
+	HOST = 'cs425-g45-' + vmNum + '.cs.illinois.edu'
+	print HOST
+	PORT = 12345
+	print PORT
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	print 'Connecting to {one} at port {two}'.format(one=HOST,two=PORT)
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((HOST, PORT))
 	s.sendall(b'Hello world')
 	data = s.recv(1024)
 
-print('Received', repr(data))
+	print('Received', repr(data))
