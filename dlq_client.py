7216a16d2bb53c4cd9c45060206948d40e414cdf
tanqiul <liutanqiu@gmail.com>
2018-09-12 23:26:50 -0500

    parallel client

--- dlq_client.py
+++ dlq_client.py
@@ -1,32 +1,55 @@
 import socket
 import sys
 from Socket import *
+from functools import reduce
 
 SERVER_PORT = 12345
-BUF_SIZE = 4096
+MSGLEN = 4096
 
 if __name__ == "__main__":
-    node = sys.argv[1]
-    pattern = sys.argv[2]
-    filename = sys.argv[3]
+    # node = sys.argv[1]
+    pattern = sys.argv[1]
+    filename = sys.argv[2]
 
-    s = TCPSocket()
-    try:
-        s.connect((node, SERVER_PORT))
-        m = ' '.join([pattern, filename])
-        s.send(m.encode())
-        # recv
-        chunks = []
-        while True:
-            if s.activityDetected(5):
-                chunk = s.recv(BUF_SIZE)
-                if chunk == b'':
-                    break
-                chunks.append(chunk)
-            else:
-                break
-        rtn_msg = b''.join(chunks)
-
-        print(rtn_msg.decode())
-    except ConnectionRefusedError as e:
-        print(str(e) + ': ' + node)
+    # nodes = ['127.0.0.1', '18.218.231.95']
+    nodes = {'localhost':'127.0.0.1' , 'aws-ec2':'18.218.231.95'}
+    socks = {}
+    for node in nodes:
+        try:
+            socks[node] = TCPSocket()
+            s = socks[node]
+            s.connect((nodes[node], SERVER_PORT))
+            m = ' '.join([pattern, filename])
+            s.send(m.encode())
+        except ConnectionRefusedError as e:
+            print(str(e) + ': ' + nodes[node])
+            nodes.pop(node)
+            if node in socks:
+                socks.pop(node)
+    buffers = {node:'' for node in nodes}
+    finish_flag = {node:False for node in nodes}
+    counts = {node:0 for node in nodes}
+    while True:
+        for node in nodes:
+            try:
+                if socks[node].activityDetected(5):
+                    chunk = socks[node].recv(MSGLEN).decode()
+                    if chunk == '':
+                        finish_flag[node] = True
+                        continue
+                    buffers[node] += chunk
+                    records = buffers[node].split('\n')
+                    for i in range(len(records) - 1):
+                        print(nodes[node] + ': ' + records[i])
+                        counts[node] += 1
+                    buffers[node] = records[-1]
+                else:
+                    finish_flag[node] = True
+                    continue
+            except ConnectionRefusedError as e:
+                print(str(e) + ': ' + node)
+                finish_flag[node] = True
+        #print(finish_flag)
+        if reduce((lambda x,y:x or y), [finish_flag[node] for node in nodes]):
+            print(counts)
+            break
